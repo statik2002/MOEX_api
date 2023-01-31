c2bf4ecb1654e11f2b4a260f49544e3d2b1ac569
statik2002 <statik2002@mail.ru>
2023-01-31 11:13:31 +0800

    Считывание json из API + DataFrame + график
    1. Dict ложим в DataFrame
    2. Оставляем в DataFrame только TQBR и данные без NaN
    3. Вывод графика

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from pprint import pprint
 
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
 
 def get_ticket_history(ticker, from_date, till_date):
@@ -22,9 +23,18 @@ def get_ticket_history(ticker, from_date, till_date):
 
 history = get_ticket_history('MOEX', '2022-01-01', '2023-01-01')
 
-pprint(history)
+columns = ['BOARDID', 'TRADEDATE', 'SHORTNAME', 'SECID', 'NUMTRADES', 'VALUE', 'OPEN', 'LOW', 'HIGH', 'LEGALCLOSEPRICE', 'WAPRICE', 'CLOSE', 'VOLUME', 'MARKETPRICE2', 'MARKETPRICE3', 'ADMITTEDQUOTE', 'MP2VALTRD','MARKETPRICE3TRADESVALUE', 'ADMITTEDVALUE', 'WAVAL', 'TRADINGSESSION']
 
-dataframe = pd.DataFrame.from_dict(history['history']['data'], columns=history['history']['columns'], orient='index')
+dataframe = pd.DataFrame.from_dict(history['history']['data'])
+dataframe.columns = columns
 
 print(dataframe)
 
+df2 = dataframe[['OPEN', 'LOW', 'HIGH', 'CLOSE']].copy()
+
+df3 = dataframe.loc[((dataframe['BOARDID'] == 'TQBR') & (dataframe['CLOSE']))]
+
+print(df3)
+
+plt.plot(df3['CLOSE'])
+plt.show()
